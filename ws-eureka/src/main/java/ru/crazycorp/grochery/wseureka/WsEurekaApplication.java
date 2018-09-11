96cc970031b0e852d1aae7fcecf60adbf3bc1453
DanKantimirov <kantimirov_da@yahoo.com>
2018-09-11 23:01:25 +0300

    Added 3 modules

---
+++ ws-eureka/src/main/java/ru/crazycorp/grochery/wseureka/WsEurekaApplication.java
@@ -0,0 +1,14 @@
+package ru.crazycorp.grochery.wseureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class WsEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsEurekaApplication.class, args);
+	}
+}
---
+++ ws-eureka/src/test/java/ru/crazycorp/grochery/wseureka/WsEurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.crazycorp.grochery.wseureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WsEurekaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
