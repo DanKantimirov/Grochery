1193d81e914a30b92354c4683a46008b954991fc
DanKantimirov <kantimirov_da@yahoo.com>
2018-12-15 20:08:36 +0300

    Сделал тесты на ProductService

--- ws-product/src/test/java/ru/crazycorp/grochery/wsproduct/service/ProductServiceTest.java
+++ ws-product/src/test/java/ru/crazycorp/grochery/wsproduct/service/ProductServiceTest.java
@@ -3,6 +3,7 @@ package ru.crazycorp.grochery.wsproduct.service;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
+import org.assertj.core.util.Arrays;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -12,6 +13,8 @@ import org.mockito.junit.MockitoJUnitRunner;
 import ru.crazycorp.grochery.wsproduct.entity.database.Product;
 import ru.crazycorp.grochery.wsproduct.repository.ProductRepository;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.Assert.*;
@@ -33,6 +36,38 @@ public class ProductServiceTest {
         testProduct.setProductName("TestPoduct");
         when(productRepository.findById(1)).thenReturn(Optional.of(testProduct));
         Product product = productService.getProductById(1);
+        assertEquals(testProduct.getProductId(), product.getProductId());
+        assertEquals(testProduct.getProductName(), product.getProductName());
+    }
+
+    @Test
+    public void getAllProducts() {
+
+        Product testProduct1 = new Product();
+        testProduct1.setProductId(1);
+        testProduct1.setProductName("TestProduct1");
+
+        Product testProduct2 = new Product();
+        testProduct2.setProductId(2);
+        testProduct2.setProductName("TestProduct2");
+
+        List<Product> testProductList = new ArrayList<>();
+        testProductList.add(testProduct1);
+        testProductList.add(testProduct2);
+
+        when(productRepository.findAll()).thenReturn(testProductList);
+
+        List<Product> productList = productService.getAllProducts();
+        assertArrayEquals(testProductList.toArray(), productList.toArray());
+    }
+
+    @Test
+    public void addProduct() {
+        Product testProduct = new Product();
+        testProduct.setProductId(777);
+        testProduct.setProductName("TestPoduct");
+        when(productRepository.save(testProduct)).thenReturn(testProduct);
+        Product product = productService.addProduct(testProduct);
         assertEquals(testProduct.getProductId(), product.getProductId());
         assertEquals(testProduct.getProductName(), product.getProductName());
     }
